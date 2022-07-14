9cf39a54f351d1ffa84a85e731abc3f18f0deb76
Taciana Shimokawa <tmshimokawa@gmail.com>
2022-07-14 20:17:06 -0300

    rails news

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-100.times do |i|
+require 'faker'
 
-  Restaurant.create(
-    name: Faker::Restaurant.name,
-    address: Faker::Address.street_address,
-  )
+case Rails.env
+  when "development"
+  5.times do |i|
+    restaurant = Restaurant.create!(
+      name: Faker::Restaurant.name,
+      address: Faker::Address.street_address,
+      phone_number: Faker::PhoneNumber.phone_number,
+      category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    )
+    5.times do
+      Review.create!(
+        rating: rand(0..5),
+        content: Faker::Movies::BackToTheFuture.quote,
+        restaurant: restaurant
+      )
+    end
+  end
+end
+
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
