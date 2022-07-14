cacf709bd9d951d813416a28561b7b8d672dcb56
Taciana Shimokawa <tmshimokawa@gmail.com>
2022-07-14 19:03:19 -0300

    rails new

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,26 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurant = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
---
+++ db/migrate/20220713011832_add_attributes_to_restaurants.rb
@@ -0,0 +1,5 @@
+class AddAttributesToRestaurants < ActiveRecord::Migration[6.1]
+  def change
+    add_column :restaurants, :phone_number, :string
+  end
+end
